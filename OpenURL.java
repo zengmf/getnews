e022e99aa9f85f4e1aaaf71475a7c819c99bb335
zengmf <46768649+zengmf@users.noreply.github.com>
2019-03-04 15:03:20 +0800

    Add files via upload

---
+++ OpenURL.java
@@ -0,0 +1,29 @@
+package geturl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+ 
+public class OpenURL {
+	/**
+	 * 获取网页上的源码
+	 * 
+	 * @throws IOException
+	 */
+	public static String get(URL url) throws IOException {
+		String times = null;
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setConnectTimeout(5 * 1000);
+		InputStream read = conn.getInputStream();
+		byte[] infor = new byte[1024];
+		int len = 0;
+		while ((len = read.read(infor)) != -1) {
+			// 指定网页的编码格式“gbk"，防止乱码
+			times += new String(infor, 0, len, "gbk");
+		}
+		read.close();
+		return times;
+	}
+}
